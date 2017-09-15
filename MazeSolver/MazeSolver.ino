549e29b37938c69d447e7c766d9e864738681951
Rosalind Ellis <rnellis@brandeis.edu>
2017-09-15 13:56:18 -0400

    sensors library

--- MazeSolver/MazeSolver.ino
+++ MazeSolver/MazeSolver.ino
@@ -1,77 +1,90 @@
 #include "MeOrion.h"
-
-const int DataPin = 12;
-uint8_t Sensor_Data[3];
-uint8_t pins[6];
-
-void setup() 
-{
-    Serial.begin(9600);
-    delay(100);
-    Serial.println("Start.....");
-}
-
-void loop()
-{
-//    Serial.println(getValue(),BIN);
-    checkLine();
-    delay(100);
-}
-
-uint8_t getValue()
-{  
-    long time_out_flag = 0;
-    pinMode(DataPin, OUTPUT);
-    digitalWrite(DataPin, LOW);
-    delayMicroseconds(980);
-    digitalWrite(DataPin, HIGH);
-    delayMicroseconds(40);
-    pinMode(DataPin, INPUT_PULLUP);
-    delayMicroseconds(50); 
-    time_out_flag = millis();
-    while((digitalRead(DataPin) == 0)&&((millis() - time_out_flag) < 6)); 
-    time_out_flag = millis();
-    while((digitalRead(DataPin) == 1)&&((millis() - time_out_flag) < 6));
-    for(uint8_t k=0; k<3; k++)
-    {
-        Sensor_Data[k] = 0x00;
-        for(uint8_t i=0;i<8;i++)
-        {
-            time_out_flag = millis(); 
-            while(digitalRead(DataPin) == 0&&((millis() - time_out_flag) < 6));
-            uint32_t HIGH_level_read_time = micros();
-            time_out_flag = millis(); 
-            while(digitalRead(DataPin) == 1&&((millis() - time_out_flag) < 6));
-            HIGH_level_read_time = micros() - HIGH_level_read_time;
-           if(HIGH_level_read_time > 50 && HIGH_level_read_time < 100)  
-            {
-                Sensor_Data[k] |= (0x80 >> i);
-            }
-        }
-    }
-    if (Sensor_Data[1] == (uint8_t)(~(uint8_t)Sensor_Data[0]))
-    {
-       return Sensor_Data[0];
-    }
-}
-
-void checkLine()
-{
-  getBin(getValue());
-}
-
-void getBin(uint8_t dec) {
-  String binary = String(dec, BIN);
-  int bin = 5; // length of pin bits
-  int binaryLength = binary.length();
-  while (bin > -1) {
-    if (binaryLength > 0) {
-      pins[bin] = binary.charAt(binaryLength - 1) - 48;
-      binaryLength--;
-    } else {
-      pins[bin] = 0;
-    }
-    bin--;
+  /**
+   * VARIABLES
+   */
+    uint8_t pins[6]; // sensor information - for more complex line correction
+    String pinString; // for simple case - turns, forward
+    const int DataPin = 12;
+    uint8_t Sensor_Data[3];
+  
+  void setup() 
+  {
+      Serial.begin(9600);
+      delay(100);
+      Serial.println("Start.....");
   }
-}
+
+  void loop()
+  {
+    getBin(getValue());
+    Serial.println(pinString);
+  }
+
+  
+  
+  /**
+   * Provided by Jonathan Maeda, gets the decimal value from the sensors
+   */
+  uint8_t getValue()
+  {  
+      long time_out_flag = 0;
+      pinMode(DataPin, OUTPUT);
+      digitalWrite(DataPin, LOW);
+      delayMicroseconds(980);
+      digitalWrite(DataPin, HIGH);
+      delayMicroseconds(40);
+      pinMode(DataPin, INPUT_PULLUP);
+      delayMicroseconds(50); 
+      time_out_flag = millis();
+      while((digitalRead(DataPin) == 0)&&((millis() - time_out_flag) < 6)); 
+      time_out_flag = millis();
+      while((digitalRead(DataPin) == 1)&&((millis() - time_out_flag) < 6));
+      for(uint8_t k=0; k<3; k++)
+      {
+          Sensor_Data[k] = 0x00;
+          for(uint8_t i=0;i<8;i++)
+          {
+              time_out_flag = millis(); 
+              while(digitalRead(DataPin) == 0&&((millis() - time_out_flag) < 6));
+              uint32_t HIGH_level_read_time = micros();
+              time_out_flag = millis(); 
+              while(digitalRead(DataPin) == 1&&((millis() - time_out_flag) < 6));
+              HIGH_level_read_time = micros() - HIGH_level_read_time;
+             if(HIGH_level_read_time > 50 && HIGH_level_read_time < 100)  
+              {
+                  Sensor_Data[k] |= (0x80 >> i);
+              }
+          }
+      }
+      if (Sensor_Data[1] == (uint8_t)(~(uint8_t)Sensor_Data[0]))
+      {
+         return Sensor_Data[0];
+      }
+  }
+  
+  /**
+   * Converts decimal from getValue to a binary array
+   */
+  void getBin(uint8_t dec) {
+    String binary = String(dec, BIN); // string conversion provided by arduino
+    pinString = binary;
+    int bin = 5; // length of pin bits
+    int binaryLength = binary.length();
+    if (binaryLength < 6) {
+      int numOfZeros = 6 - binaryLength;
+      for (int i = 0; i <= numOfZeros; i++) {
+        binary = "0" + binary;
+      }
+    }
+    // put into array (have to account for 0s in the front
+    while (bin > -1) {
+      if (binaryLength > 0) {
+        pins[bin] = binary.charAt(binaryLength - 1) - 48;
+        binaryLength--;
+      } else { // account for front zeros
+        pins[bin] = 0;
+      }
+      bin--;
+    }
+  }
 
---
+++ Sensors/Sensors.h
@@ -0,0 +1,24 @@
+#ifndef Sensors_h
+#define Sensors_h
+
+#include "MeOrion.h"
+
+class Sensors {
+
+  public:
+    uint8_t[] getCorrectionInfo();
+    String getStringBin();
+
+  private:
+    const int DataPin = 12;
+    uint8_t Sensor_Data[3];
+    //Sensor Information
+    uint8_t correctionPins[6];
+    String pinString; 
+    getValue();
+    getBin();
+  
+}
+#endif
+
+
