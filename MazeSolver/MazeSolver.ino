0272035da3293c84a3f027bd81a11e79bcab351d
Rosalind Ellis <rnellis@brandeis.edu>
2017-09-18 12:54:49 -0400

    using line array now

--- MazeSolver/MazeSolver.ino
+++ MazeSolver/MazeSolver.ino
@@ -1,5 +1,7 @@
 
 #include "MeOrion.h"
+#include "LineArray.h"
+
 
 // MOTOR STUFF
 MeDCMotor motor1(M1);
@@ -9,6 +11,7 @@ int t;
 bool done;
 
 // SENSOR STUFF
+LineArray lineArray;
 uint8_t pins[6];
 uint8_t Sensor_Data[3];
 const int DataPin = 12;
@@ -16,6 +19,7 @@ const int DataPin = 12;
 void setup() 
 {
   Serial.begin(9600);
+  lineArray.setPort(12);
   motorSpeed = 60;
   t = 1;
   done = false;
@@ -25,14 +29,15 @@ void setup()
 
 void loop()
 {
-  getBin(getValue());
+//  getBin(getValue());
+  getBinArray(lineArray.getBINValue());
   printPins();
-  if (!done) {
-    checkMovement();
-  } else {
-    motor1.stop();
-    motor2.stop();
-  }
+//  if (!done) {
+//    checkMovement();
+//  } else {
+//    motor1.stop();
+//    motor2.stop();
+//  }
 }
 
 void printPins() {
@@ -47,62 +52,13 @@ void printPins() {
 /**
  * SENSOR METHODS
  */
- 
-/**
- * Provided by Jonathan Maeda, gets the decimal value from the sensors
- */
-uint8_t getValue()
-{  
-  long time_out_flag = 0;
-  pinMode(DataPin, OUTPUT);
-  digitalWrite(DataPin, LOW);
-  delayMicroseconds(980);
-  digitalWrite(DataPin, HIGH);
-  delayMicroseconds(40);
-  pinMode(DataPin, INPUT_PULLUP);
-  delayMicroseconds(50); 
-  time_out_flag = millis();
-  while((digitalRead(DataPin) == 0)&&((millis() - time_out_flag) < 6)); 
-  time_out_flag = millis();
-  while((digitalRead(DataPin) == 1)&&((millis() - time_out_flag) < 6));
-  for(uint8_t k=0; k<3; k++)
-  {
-   Sensor_Data[k] = 0x00;
-   for(uint8_t i=0;i<8;i++)
-   {
-     time_out_flag = millis(); 
-     while(digitalRead(DataPin) == 0&&((millis() - time_out_flag) < 6));
-     uint32_t HIGH_level_read_time = micros();
-     time_out_flag = millis(); 
-     while(digitalRead(DataPin) == 1&&((millis() - time_out_flag) < 6));
-     HIGH_level_read_time = micros() - HIGH_level_read_time;
-     if(HIGH_level_read_time > 50 && HIGH_level_read_time < 100)  
-     {
-       Sensor_Data[k] |= (0x80 >> i);
-     }
-   }
-  }
-  if (Sensor_Data[1] == (uint8_t)(~(uint8_t)Sensor_Data[0]))
-  {
-  return Sensor_Data[0];
-  }
-}
 
 /**
 * Converts decimal from getValue to a binary array
 */
-void getBin(uint8_t dec) {
-  String binary = String(dec, BIN);
-  int bin = 0; // length of pin bits
-  int binaryLength = binary.length();
-  while (bin < 6) {
-    if (binaryLength > 0) {
-      pins[bin] = binary.charAt(binaryLength - 1) - 48;
-      binaryLength--;
-    } else {
-      pins[bin] = 0;
-    }
-    bin++;
+void getBinArray(String stringBin) {
+  for (int i = 0; i < 6; i++) {
+    pins[i] = stringBin.charAt(5 - i) - 48;
   }
 }
 
